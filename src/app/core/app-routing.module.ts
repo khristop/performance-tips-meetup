ea0a221dfb4fcd8464a3bcc62269436f4912e2a0
Kevin Garcia <kgarci@acxiom.com>
2021-05-01 15:06:34 -0600

    :sparkles: Change menu options

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,35 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    children: [
-      {
-        path: 'change-detection',
-        loadChildren: () => import('../change-detection-example/change-detection-example.module').then(m => m.ChangeDetectionExampleModule)
-      },
-      {
-        path: 'pipes',
-        loadChildren: () => import('../pipes-example/pipes-example.module').then(m => m.PipesExampleModule)
-      },
-      {
-        path: 'trackby',
-        loadChildren: () => import('../track-by-example/track-by-example.module').then(m => m.TrackByExampleModule)
-      },
-      {
-        path: 'virtual-scroll',
-        loadChildren: () => import('../virtual-scroll-example/virtual-scroll-example.module').then(m => m.VirtualScrollExampleModule)
-      },
-    ]
-  }
+    path: "",
+    redirectTo: "change-detection",
+    pathMatch:"full"
+  },
+  {
+    path: "change-detection",
+    loadChildren: () =>
+      import(
+        "../change-detection-example/change-detection-example.module"
+      ).then((m) => m.ChangeDetectionExampleModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [
-    RouterModule
-  ]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
